282e725cd6c38ef0972637cebc251211f851394e
HerozDotExe <herozdotexe@gmail.com>
2026-03-08 12:47:44 +0100

    build: set base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/nlk-commands-comparer/",
     plugins: [tailwindcss(), svelte()],
     resolve: {
         alias: {
